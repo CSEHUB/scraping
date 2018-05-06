441c764e5911844dbb099a9f75b987eea718e527
ericping1 <eric.ping09@gmail.com>
2018-05-06 16:17:07 -0700

    Finished working version of cape scraper

--- capeScraper.py
+++ capeScraper.py
@@ -1,12 +1,21 @@
-import requests
 from bs4 import BeautifulSoup
+from tabulate import tabulate
+import requests
+import pandas as pd
 
+
+# Set website and user-agent or page will obfuscate datea
 cape_website = "http://www.cape.ucsd.edu/responses/Results.aspx?Name=cse+110&CourseNumber="
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-request = requests.get(cape_website)
+# Set up requests BeautifulSoup scraper
+request = requests.get(cape_website, headers=headers)
+soup = BeautifulSoup(request.content, 'html.parser')
 
-soup = BeautifulSoup(request.text, 'html.parser')
+# Generate table
+table = soup.find_all('table')[0]
+df = pd.read_html(str(table))
 
-prettyString = soup.prettify().encode('utf-8').strip()
 
-print (prettyString)
+print(tabulate(df[0], headers='keys', tablefmt='psql'))
+
